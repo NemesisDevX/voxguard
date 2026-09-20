32a5d7d4582c5a2571ad7ad55c3ab2dd73610110
NemesisDevX <NemesisDevX@users.noreply.github.com>
2026-09-20 18:00:04 +0000

    deploy: voxguard web build dfb5606b0b2f571fcbebd48efb6fecfa3a9f660e dfb5606b0b2f571fcbebd48efb6fecfa3a9f660e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1424914093" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3590040016" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
